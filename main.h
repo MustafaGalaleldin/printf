152ba2865df1c75114776e4333f8ede2747a7bd3
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-14 06:40:04 -0800

    header file

--- main.h
+++ main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stddef.h>
-
 int _mchar(char c);
 int _printf(const char *format, ...);
-int handle_case(char format, va_list *type, int *count, size_t i, const char *s);
+int handle_case(char format, va_list *type, int *count, int i, const char *s);
 int hxh_char(va_list *type, int *count);
 int hxh_str(va_list *type, int *count);
 int hxh_binary(va_list *type, int *count);
@@ -19,13 +17,15 @@ int hxh_M_hexa(va_list *type, int *count);
 int hxh_unsigned(va_list *type, int *count);
 int hxh_print_unsign(unsigned int n, int *count);
 int hxh_Str(va_list *ptr, int *count);
-
-typedef struct
+/**
+ * struct functions - functions structure
+ * @specifier: char specifier
+ * @handler: pointer to function
+ */
+typedef struct functions
 {
 	char specifier;
 	int (*handler)(va_list *, int *);
 } Format_specifier;
-
 extern Format_specifier specifiers[];
-
 #endif
