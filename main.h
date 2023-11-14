df7523519eebce4d8fd6e4067d3318aa0704f552
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-14 16:24:39 +0200

    main.h

---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+int _mchar(char c);
+int _printf(const char *format, ...);
+int handle_case(char format, va_list *type, int *count, size_t i, const char *s);
+int hxh_char(va_list *type, int *count);
+int hxh_str(va_list *type, int *count);
+int hxh_binary(va_list *type, int *count);
+int hxh_int(va_list *type, int *count);
+int hxh_octal(va_list *type, int *count);
+int hxh_print_number(int n, int *count);
+int hxh_m_hexa(va_list *type, int *count);
+int hxh_M_hexa(va_list *type, int *count);
+int hxh_unsigned(va_list *type, int *count);
+int hxh_print_unsign(unsigned int n, int *count);
+int hxh_Str(va_list *ptr, int *count);
+
+typedef struct
+{
+	char specifier;
+	int (*handler)(va_list *, int *);
+} Format_specifier;
+
+extern Format_specifier specifiers[];
+
+#endif
