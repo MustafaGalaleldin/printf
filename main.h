824edafc3ec54b7f68917ea52094387190e34c14
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-15 01:11:03 +0200

    modified the header file

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
+include <stddef.h>
 #include <stdarg.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _mchar(char c);
 int _printf(const char *format, ...);
