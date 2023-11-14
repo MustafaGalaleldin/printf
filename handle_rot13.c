2fd7c98ec843ce6cafce403a9fd3a5180abe9c07
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-14 15:16:09 -0800

    rot13 bug fixed

--- handle_rot13.c
+++ handle_rot13.c
@@ -13,17 +13,25 @@ int hxh_rot13(va_list *type, int *count)
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghigklm";
 	int i, j;
-	char *ptr = va_arg(*type, char *);
+	char *p = va_arg(*type, char *);
 
-	for (i = 0; ptr[i]; i++)
+	for (i = 0; p[i]; i++)
 	{
-		for (j = 0; j < 52; j++)
+		if ((p[i] > 64 && p[i] < 91) || (p[i] > 96 && p[i] < 123))
 		{
-			if (ptr[i] == a[j])
+			for (j = 0; j < 52; j++)
 			{
-				_mchar(b[j]);
-				(*count)++;
+				if (p[i] == a[j])
+				{
+					_mchar(b[j]);
+					(*count)++;
+				}
 			}
+		}
+		else
+		{
+			_mchar(p[i]);
+			(*count)++;
 		}
 	}
 	return (*count);
